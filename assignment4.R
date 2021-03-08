a098d779a3cfd484de007d15780ee277d1e2b757
toddnoletto <78041262+toddnoletto@users.noreply.github.com>
2021-03-08 12:50:20 -0500

    Add files via upload

---
+++ assignment4.R
@@ -0,0 +1,39 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("curl")
+library(curl)
+
+install.packages("MRIcloudT1volumetrics")
+library(devtools)
+
+MRIcloudT1volumetrics::readSubject("https://raw.githubusercontent.com/neuroconductor/kirby21.mricloud/master/inst/visit_1/127/kirby_3_1_ax_283Labels_M2_corrected_stats.txt")
+
+read_csv("https://raw.githubusercontent.com/jhu-advdatasci/2018/master/data/KFF/healthcare-spending.csv", skip = 2,  n_max = 52)
+
+read_csv("https://raw.githubusercontent.com/jhu-advdatasci/2018/master/data/KFF/healthcare-spending.csv", skip = 2,  n_max = 52)
+
+
+devtools::install_github("bcaffo/kirby21.mricloud")
+
+install.packages("kirby21.base")
+
+
+MRIcloudT1volumetrics::readFileList("https://raw.githubusercontent.com/neuroconductor/kirby21.mricloud/master/inst/visit_1/127/kirby_3_1_ax_283Labels_M2_corrected_stats.txt")
+
+library(kirby21.base)
+
+library(kirby21.mricloud)
+
+install.packages("remotes")
+remotes::install_github("neuroconductor/MRIcloudT1volumetrics")
+
+
+dat= MRIcloudT1volumetrics::raedFileList("https://raw.githubusercontent.com/neuroconductor/kirby21.mricloud/master/inst/visit_1/127/kirby_3_1_ax_283Labels_M2_corrected_stats.txt")
+
+
+
+
+
+
+
